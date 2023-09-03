4b3c87ba914f760a944c868e8eb8788d90ec4a20
Iamneo <techadmin@iamneo.ai>
2023-09-03 08:44:05 +0000

    Backup Commit

---
+++ public/tic.js
@@ -0,0 +1,24 @@
+function myfunc(){
+    var b1,b2,b3,b4,b5,b6,b7,b8,b9;
+    b1=document.getElementById("b1").value;
+    b2=document.getElementById("b2").value;
+    b3=document.getElementById("b3").value;
+    b4=document.getElementById("b4").value;
+    b5=document.getElementById("b5").value;
+    b6=document.getElementById("b6").value;
+    b7=document.getElementById("b7").value;
+    b8=document.getElementById("b8").value;
+    b9=document.getElementById("b9").value;
+    var b1btn,b2btn,b3btn,b4btn,b5btn,b6btn,b7btn,b8btn,b9btn;
+    b1btn=document.getElementById("b1").value;
+    b2btn=document.getElementById("b2").value;
+    b3btn=document.getElementById("b3").value;
+    b4btn=document.getElementById("b4").value;
+    b5btn=document.getElementById("b5").value;
+    b6btn=document.getElementById("b6").value;
+    b7btn=document.getElementById("b7").value;
+    b8btn=document.getElementById("b8").value;
+    b9btn=document.getElementById("b9").value;
+    if((b1=='x' || b1==))
+
+}
